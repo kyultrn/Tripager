6a23e85fe97c2bf91a11f7e87573227aeb089302
zach <zachartydempsey@gmail.com.here>
2023-02-13 16:08:17 -0600

    mischa freaking the fuck out

---
+++ accounts/migrations/001_accounts_table.py
@@ -0,0 +1,12 @@
+# steps=[
+#     [
+#         '''
+#         CREATE TABLE accounts (
+#             id SERIAL PRIMARY KEY NOT NULL,
+#             name VARCHAR(255) NOT NULL,
+#             email VARCHAR(255) NOT NULL,
+
+#         );
+#         ''',
+#     ]
+# ]
---
+++ accounts/queries/pool.py
@@ -0,0 +1,5 @@
+# import os
+# from psycopg.pool import ConnectionPool
+
+
+# pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
