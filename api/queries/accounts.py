1d70034f6e66b341d22febd4a7fa2ec275ad6664
zach <zachartydempsey@gmail.com.here>
2023-02-14 12:28:02 -0600

    don't pull this authen

---
+++ api/queries/accounts.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class DuplicateAccountError(ValueError):
+    pass
+
+
+class AccountIn(BaseModel):
+    full_name: str
+    password: str
+    email: str
+
+class AccountOut(BaseModel):
+    id: str
+    email: str
+    full_name: str
+
+
+class AccountOutWithPassword(AccountOut):
+    hashed_password: str
+
+
+class AccountQueries(Queries): #not set up yet pool.py or something in there
+
+
+    def get(self, email: str) -> AccountOutWithPassword:
+
+    def create(self, info: AccountIn, hashed_password: str) -> AccountOut
