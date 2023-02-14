29bb04a426af7eb31dab86d0a03ebde2bdb2a85a
Kyle <dopitoya@gmail.com>
2023-02-13 20:45:04 -0500

    Finished containers and docker yaml files/ pulling zach's repo

--- events/queries/pool.py
+++
@@ -1,5 +0,0 @@
-# import os
-# from psycopg.pool import ConnectionPool
-
-
-# pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
--- relational-data/create-multiple-databases.sh
+++ relational-data/create-multiple-databases.sh
@@ -13,10 +13,4 @@ function create_user_and_database() {
 EOSQL
 }
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-        create_user_and_database $db
-    done
-    echo "Multiple databases created"
-fi
+
