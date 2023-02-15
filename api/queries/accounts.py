f1257facdf8881e0cd1d463589645e5836c5bbeb
Kyle <dopitoya@gmail.com>
2023-02-14 19:28:11 -0500

    Finished CRU for Accounts

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 from queries.pool import pool
+from typing import List, Union
+
+class Error(BaseModel):
+    message: str
 
 class AccountIn(BaseModel):
     name: str
@@ -13,32 +17,95 @@ class AccountOut(BaseModel):
     email: str
 
 class AccountRepository:
+    def update(self, account_id: int, account: AccountIn) -> Union[AccountOut, Error]:
+        try:
+            # Connect to the database
+            with pool.connection() as conn:
+                # Get a cursor [something to run SQL with]
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        UPDATE accounts
+                        SET name = %s
+                          , email = %s
+                          , password = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            account.name,
+                            account.email,
+                            account.password,
+                            account_id
+                        ]
+                    )
+                    # old_data = account.dict()
+                    return self.account_in_to_out(account_id, account)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not update account"}
+
+    def get_all(self) -> Union[Error, List[AccountOut]]:
+        try:
+            # Connect to the database
+            with pool.connection() as conn:
+                # Get a cursor [something to run SQL with]
+                with conn.cursor() as db:
+                    # Run our SELECT statement
+                    result = db.execute(
+                        """
+                        SELECT id, name, email, password
+                        FROM accounts
+                        ORDER BY id;
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        account = AccountOut(
+                            id=record[0],
+                            name=record[1],
+                            email=record[2],
+                            password=record[3]
+                        )
+                        result.append(account)
+                    return result
+        except Exception as e:
+            print(e)
+            return {"message": "Could not receive all accounts"}
+
+
     def create(self, account: AccountIn) -> AccountOut:
-        # Connect to the database
-        with pool.connection() as conn:
-            # Get a cursor [something to run SQL with]
-            with conn.cursor() as db:
-                # Run our INSERT statement
-                result = db.execute(
-                    """
-                    INSERT INTO accounts
-                        (name, email, password)
-                    VALUES
-                        (%s, %s, %s)
-                    RETURNING ID;
-                    """,
-                    [
-                        account.name,
-                        account.email,
-                        account.password
-                    ]
-                )
-                id = result.fetchone()[0]
-                # Return new data
-                old_data = account.dict()
-                return AccountOut(id=id, **old_data)
+        try:
+            # Connect to the database
+            with pool.connection() as conn:
+                # Get a cursor [something to run SQL with]
+                with conn.cursor() as db:
+                    # Run our INSERT statement
+                    result = db.execute(
+                        """
+                        INSERT INTO accounts
+                            (name, email, password)
+                        VALUES
+                            (%s, %s, %s)
+                        RETURNING ID;
+                        """,
+                        [
+                            account.name,
+                            account.email,
+                            account.password
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    # Return new data
+                    # old_data = account.dict()
+                    # return AccountOut(id=id, **old_data)
+                    return self.account_in_to_out(id, account)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not create account"}
 
-
+    def account_in_to_out(self, id: int, account: AccountIn):
+        old_data = account.dict()
+        return AccountOut(id=id, **old_data)
 
 
 # class DuplicateAccountError(ValueError):
--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -1,11 +1,35 @@
-from fastapi import APIRouter, Depends
-from queries.accounts import AccountIn, AccountRepository
+from fastapi import APIRouter, Depends, Response
+from typing import Union, List
+from queries.accounts import(
+    Error,
+    AccountIn,
+    AccountRepository,
+    AccountOut,
+)
 
 router = APIRouter()
 
-@router.post("/accounts")
+@router.post("/accounts", response_model=Union[AccountOut, Error])
 def create_account(
     account: AccountIn,
-    repo: AccountRepository = Depends()
+    response: Response,
+    repo: AccountRepository = Depends(),
 ):
+    response.status_code = 400
     return repo.create(account)
+
+
+@router.get("/accounts", response_model=List[AccountOut])
+def get_all(
+    repo: AccountRepository = Depends(),
+):
+    return repo.get_all()
+
+
+@router.put("/accounts/{account_id}", response_model=Union[AccountOut, Error])
+def update_account(
+    account_id: int,
+    account: AccountIn,
+    repo: AccountRepository = Depends(),
+) -> Union[Error, AccountOut]:
+    return repo.update(account_id, account)
