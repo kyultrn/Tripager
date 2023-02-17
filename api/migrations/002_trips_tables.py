6233c6cfa662c0b2627a179f25b311024b3fdd04
zach <zachartydempsey@gmail.com.here>
2023-02-17 14:50:54 -0600

    changed tables

---
+++ api/migrations/002_trips_tables.py
@@ -0,0 +1,17 @@
+steps=[
+    [
+        """
+        CREATE TABLE trips (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            city VARCHAR(255) NOT NULL,
+            state VARCHAR(255) NOT NULL,
+            start_date DATE NOT NULL,
+            end_date DATE NOT NULL
+        );
+        """,
+        """
+        DROP TABLE trips;
+        """
+    ]
+]
