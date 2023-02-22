a1752280bc96ce2b0bb34c6ea610e3a8e94835f9
Mischa <michelle.d.goodman@gmail.com>
2023-02-22 12:19:23 -0500

    New login/logout branch

---
+++ ghi/src/MainPage.js
@@ -0,0 +1,15 @@
+
+function MainPage() {
+    return  (
+        <div>
+            <h1>Tripager</h1>
+            <h2>Plan and manage your next trip here!</h2>
+
+        </div>
+    );
+}
+
+export default MainPage
+
+
+
