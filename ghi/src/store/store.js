60bfbb79554e887ab2edb9ff33f30b37012a7086
Mischa <michelle.d.goodman@gmail.com>
2023-02-23 10:58:56 -0500

    Started the redux stuff -- store, tripsApi

---
+++ ghi/src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { tripsApi } from "./tripsApi";
+
+
+export const store = configureStore({
+  reducer: {
+    [tripsApi.reducerPath]: tripsApi.reducer,
+  },
+  middleware: getDefaultMiddleware =>
+  getDefaultMiddleware().concat(tripsApi.middleware),
+
+});
+
+setupListeners(store.dispatch)
---
+++ ghi/src/store/tripsApi.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const tripsApi = createApi({
+  reducerPath: "trips",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_ACCOUNTS_HOST,
+    // why i think should likely be called TRIPAGER
+  }),
+  endpoints: (builder) => ({
+    // Names of methods we want to use to interact with API
+    getTrips: builder.query({
+      query: () => "/api/trips/",
+    }),
+  }),
+});
+
+export const { useGetTripsQuery } = tripsApi
