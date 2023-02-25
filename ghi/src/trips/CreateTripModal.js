3c7e93b1851f24cc30af25d941faeb13438e9ce2
Kyle <dopitoya@gmail.com>
2023-02-24 23:37:38 -0500

    end date and start date now working and submit works

--- ghi/src/trips/CreateTripModal.js
+++ ghi/src/trips/CreateTripModal.js
@@ -43,71 +43,17 @@ function ModalForm() {
             <Form.Label>State</Form.Label>
             <Form.Control type='text' name='state' value={formData.state} onChange={handleInputChange} />
             <Form.Label>Start Date</Form.Label>
-            <Form.Control type='text' name='start date' value={formData.start_date} onChange={handleInputChange} />
+            <Form.Control type='text' name='start_date' value={formData.start_date} onChange={handleInputChange} />
             <Form.Label>End Date</Form.Label>
-            <Form.Control type='text' name='end date' value={formData.end_date} onChange={handleInputChange} />
+            <Form.Control type='text' name='end_date' value={formData.end_date} onChange={handleInputChange} />
             <Modal.Footer>
               <Button onClick={handleCloseModal}>Create</Button>
             </Modal.Footer>
           </Form>
         </Modal.Body>
       </Modal>
-      {/* <div className="modal-content">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            City:
-            <input
-              type="text"
-              name="city"
-              value={formData.city}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            State:
-            <input
-              type="text"
-              name="state"
-              value={formData.state}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            Start Date:
-            <input
-              type="text"
-              name="start_date"
-              value={formData.start_date}
-              onChange={handleInputChange}
-            />
-          </label>
-          <label>
-            End Date:
-            <input
-              type="text"
-              name="end_date"
-              value={formData.end_date}
-              onChange={handleInputChange}
-            />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
       </div>
-      <button
-        className="modal-close is-large"
-        aria-label="close"
-        onClick={handleCloseModal}
-      ></button> */}
-    </div>
+
   );
 }
 
