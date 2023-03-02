4e96a7a6e76959e934cd4daa94235ccb519bc59d
zach <zachartydempsey@gmail.com.here>
2023-03-02 16:06:40 -0600

    added delete function for events

--- ghi/src/events/Events.js
+++ ghi/src/events/Events.js
@@ -2,8 +2,11 @@ import { useGetEventsQuery } from "../store/ApiSlice";
 import { useGetTripQuery } from "../store/ApiSlice";
 import { tripsApi } from "../store/ApiSlice";
 import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useDeleteEventMutation } from "../store/ApiSlice";
 
 export default function Events() {
+  const [deleteEvent, { deleteError }] = useDeleteEventMutation();
   const { id } = useParams();
   const {
     data: trip,
@@ -17,14 +20,21 @@ export default function Events() {
     isLoading: tripLoading,
   } = useGetTripQuery(id);
 
+  const handleDeleteEvent = async (eventId) => {
+    try {
+      const response = await deleteEvent(eventId);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (isLoading || tripLoading) {
     return <progress className="progress is-primary" max="100"></progress>;
   } else {
     return (
       <div>
-        <h1>
-          Events for {trip.name}
-        </h1>
+        <h1>Events for {trip.name}</h1>
         <table className="table is-striped">
           <thead>
             <tr>
@@ -46,6 +56,32 @@ export default function Events() {
                 <td>{event.location}</td>
                 <td>{event.start_time}</td>
                 <td>{event.end_time}</td>
+                <i
+                  variant="btn-sm m-1"
+                  className="btn-red btn-sm text-right"
+                  onClick={() => {
+                    Swal.fire({
+                      title: "Are you sure?",
+                      text: "You won't be able to revert this!",
+                      icon: "warning",
+                      showCancelButton: true,
+                      confirmButtonColor: "#bb7e74",
+                      cancelButtonColor: "#808080",
+                      confirmButtonText: "Yes, delete it!",
+                    }).then((result) => {
+                      if (result.isConfirmed) {
+                        handleDeleteEvent(trip.id);
+                        Swal.fire(
+                          "Deleted!",
+                          "Your room has been deleted.",
+                          "success"
+                        );
+                      }
+                    });
+                  }}
+                >
+                  Delete
+                </i>
               </tr>
             ))}
           </tbody>
