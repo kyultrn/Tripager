2c0e2be59e15360f2003ba2d7b6d168dd709cf7c
Kyle <dopitoya@gmail.com>
2023-03-03 13:27:08 -0500

    added input on accounts constraint

--- api/migrations/001_accounts_tables.py
+++ api/migrations/001_accounts_tables.py
@@ -3,9 +3,9 @@ steps = [
         """
         CREATE TABLE accounts (
             id SERIAL PRIMARY KEY NOT NULL,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            hashed_password VARCHAR(250) NOT NULL
+            name VARCHAR(255) NOT NULL CHECK (name <> ''),
+            email VARCHAR(255) NOT NULL CHECK (email <> ''),
+            hashed_password VARCHAR(250) NOT NULL CHECK (hashed_password <> '')
         );
         """,
         """
