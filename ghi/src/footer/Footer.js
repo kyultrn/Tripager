ce3a4c7a5973f2d9d7055714ceb9144f6c47a175
zach <zachartydempsey@gmail.com.here>
2023-03-08 15:49:07 -0600

    fixed create event

--- ghi/src/footer/Footer.js
+++ ghi/src/footer/Footer.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
-
-  const location = useLocation()
-  const isTripsPage = ['/trips', '/login', '/signupo'].includes(location.pathname)
+  const selectedTripId = useSelector((state) => state.tripForm.selectedTripId);
+  console.log(selectedTripId);
+  const location = useLocation();
+  const isTripsPage = [
+    "/trips",
+    "/login",
+    "/signup",
+    "/myaccount",
+    `/trips/${selectedTripId}/events`,
+  ].includes(location.pathname);
 
   return (
-    <footer className="tripagerFooter my-custom-class text-white" style={{ position: isTripsPage ? "fixed" : "relative" }}>
+    <footer
+      className="tripagerFooter my-custom-class text-white"
+      style={{ position: isTripsPage ? "fixed" : "relative" }}
+    >
       <div className=" py-3">
         <div className="footer-row row">
           <div className="footer-item col-md-4 mb-3">
