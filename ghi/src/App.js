e47cc5eb400eca9a2c7eda30d4eb13546e26e143
Mischa <michelle.d.goodman@gmail.com>
2023-03-08 15:15:49 -0500

    getting ridof errors

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
-import { useToken } from "./accounts/Authenticator";
 import Login from "./accounts/Login";
-import SignUp from "./accounts/SignupModal.js";
 import Navbar from "./Nav.js";
 import TripagerHome from "./TripagerHome.js";
 import Trips from "./trips/Trips.js";
@@ -11,13 +9,9 @@ import ThingsToDo from "./things-to-do/ThingsToDo";
 import Footer from "./footer/Footer.js";
 import AboutDevelopers from "./footer/AboutDevelopers";
 import AboutTripager from "./footer/AboutTripager";
-import { setLoginState } from "./store/AccountsSlice";
-import { store } from "./store/store";
-import { useDispatch } from "react-redux";
-import VideoCarousel from "./VideoCarousel";
 
 
-function App() {
+export default function App() {
 
   return (
     <>
@@ -27,7 +21,6 @@ function App() {
           <Routes>
             <Route path="/" element={<TripagerHome />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
             <Route path="/trips" element={<Trips />} />
             <Route path="/trips/:id/events" element={<Events />} />
             <Route path="/thingstodo" element={<ThingsToDo />} />
@@ -38,7 +31,5 @@ function App() {
         <Footer />
       </BrowserRouter>
     </>
-  );
+  )
 }
-
-export default App;
