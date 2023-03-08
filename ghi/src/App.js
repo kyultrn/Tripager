ddab589914bcb618e6850d6ad349b511ce87eaf0
zach <zachartydempsey@gmail.com.here>
2023-03-08 14:16:10 -0600

    flake8

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -27,7 +27,6 @@ function App() {
           <Routes>
             <Route path="/" element={<TripagerHome />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
             <Route path="/trips" element={<Trips />} />
             <Route path="/trips/:id/events" element={<Events />} />
             <Route path="/thingstodo" element={<ThingsToDo />} />
