0af775685d63437ac885cfe329601ef142cc4328
zach <zachartydempsey@gmail.com.here>
2023-03-07 21:45:02 -0600

    footer button changes and deleted redundant repeating about <p> tags

--- ghi/src/footer/Footer.js
+++ ghi/src/footer/Footer.js
@@ -12,22 +12,19 @@ export default function Footer() {
         <div className="footer-row row">
           <div className="footer-item col-md-4 mb-3">
             <h5>About Tripager</h5>
-            <p>about our project</p>
-            <Link to="/about-tripager" className="text-white">
+            <Link to="/about-tripager" className="btn text-white">
               Learn more
             </Link>
           </div>
           <div className="footer-item col-md-4 mb-3">
             <h5>About Developers</h5>
-            <p>about our team</p>
-            <Link to="/about-developers" className="text-white">
+            <Link to="/about-developers" className="btn text-white">
               Meet the team
             </Link>
           </div>
           <div className="footer-item col-md-4 mb-3">
             <h5>Technologies</h5>
-            <p>fast-api, redux, etc</p>
-            <Link to="/technologies" className="text-white">
+            <Link to="/technologies" className="btn text-white">
               View our tech stack
             </Link>
           </div>
