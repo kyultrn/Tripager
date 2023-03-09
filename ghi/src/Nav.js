47d0dac411346064d3e496867a4ab916fb90a224
Kyle <dopitoya@gmail.com>
2023-03-09 16:23:09 -0500

    all current changes + footer changes

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { useGetTokenQuery, useUserLogoutMutation } from "./store/ApiSlice";
