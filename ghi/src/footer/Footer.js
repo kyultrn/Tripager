d2e1ed551ef2d58364899bf2bf134d72256f28fa
zach <zachartydempsey@gmail.com.here>
2023-03-09 11:25:50 -0600

    navbar and buttons

--- ghi/src/footer/Footer.js
+++ ghi/src/footer/Footer.js
@@ -5,23 +5,23 @@ import { useSelector } from "react-redux";
 export default function Footer() {
   const selectedTripId = useSelector((state) => state.tripForm.selectedTripId)
   const location = useLocation()
-  const isTripsPage = [
-    "/trips",
-    "/login",
-    "/signup",
-    "/myaccount",
-    `/trips/${selectedTripId}/events`,
-  ].includes(location.pathname)
+  // const isTripsPage = [
+  //   // "/trips",
+  //   "/login",
+  //   "/signup",
+  //   "/myaccount",
+  //   `/trips/${selectedTripId}/events`,
+  // ].includes(location.pathname)
 
   return (
     <footer
       className="tripagerFooter my-custom-class text-white"
-      style={{ position: isTripsPage ? "fixed" : "relative" }}
+      // style={{ position: isTripsPage ? "fixed" : "relative" }}
     >
       <div className=" py-3">
         <div className="footer-row row">
           <div className="footer-item col-md-4 mb-3">
-            <h5>FOOTER </h5>
+            <h5>Tripager </h5>
           </div>
         </div>
       </div>
